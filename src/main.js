6517b6f6fb3ea33ea7e190eba31331e8b27d3aef
BakshyshRamazan <o.osmanov0921@gmail.com>
2025-07-23 15:27:18 +0300

    added responsive design & added carousel plugin

--- src/main.js
+++ src/main.js
@@ -1,3 +1,10 @@
-document.getElementById("clickMe").addEventListener("click", () => {
-  alert("Button clicked!");
+// main.js или любой другой .js файл
+import $ from 'jquery';
+import "slick-carousel";
+
+$('.hero-section__carousel').slick({
+    slidesToShow: 1,
+    arrows: true,    
+    dots: false,      
+    infinite: true,
 });
